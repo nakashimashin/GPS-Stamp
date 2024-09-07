d1814038f50d4e68e5dd608c848750c30cf4d839
nakashimashin <nakashimashin0517@gmail.com>
2024-09-07 23:41:03 +0900

    prettierを追加

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 
 export default [
@@ -11,4 +12,5 @@ export default [
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  eslintConfigPrettier
 ];
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
-import Test from '@/components/Test.vue'
+import Test from '@/components/Test.vue';
 </script>
 
 <template>
-  <div>
-    <Test />
-  </div>
+	<div>
+		<Test />
+		<div>aaa</div>
+		<div>bbb</div>
+	</div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+	filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+	filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
